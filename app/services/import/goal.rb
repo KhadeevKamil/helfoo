ebc748757a3d1597821a821afc27fd4fcf25e0f1
kopylovvlad <kopylov.vlad@gmail.com>
2020-05-22 11:57:26 +0300

    import in progress

--- app/services/import/goal.rb
+++ app/services/import/goal.rb
@@ -12,12 +12,12 @@ module Import
         ['Похудеть после родов', 'pohudet'],
         ['Питаться с учетом аллергии', 'alergy']
       ].each do |goal_name, goal_slug|
-        if Goal.find_by(slug: goal_slug).present?
+        if ::Goal.find_by(slug: goal_slug).present?
           puts "Goal '#{goal_name}'/#{goal_slug} alreafy exist"
-        elsif Goal.create(title: goal_name, slug: goal_slug)
+        elsif ::Goal.create(title: goal_name, slug: goal_slug)
           puts "Created goal '#{goal_name}'/#{goal_slug}"
         else
-          puts "Can create goal '#{goal_name}'/#{goal_slug}"
+          puts "Can not create goal '#{goal_name}'/#{goal_slug}"
         end
       end
     end
--- lib/tasks/import.rake
+++ lib/tasks/import.rake
@@ -1,7 +1,18 @@
 # frozen_string_literal: true
 
 namespace :import do
-  task goals: :environment do
-    Import::Goal.call
+  task all: :environment do
+    # 01
+    Rake::Task["import:goals"].invoke
+    # 02
+    Rake::Task["import:ingredients"].invoke
+  end
+
+  task goal: :environment do
+    Import::Goal.new.call
+  end
+
+  task ingredient: :environment do
+    Import::Ingredient.new.call
   end
 end
